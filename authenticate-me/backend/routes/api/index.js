2e33f2d972744518a9bf361a1b5839295affeae8
geoffreyjiang <geoffreychjiang@gmail.com>
2022-09-06 13:27:42 -0700

    api routes

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,3 +1,12 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+
+// router.get("/please", (req, res) => {
+//   res.json("hello");
+// });
+
+router.post("/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
