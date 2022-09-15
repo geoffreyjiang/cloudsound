7e635a652d38438f914d892d8b2a0672447edb85
geoffreyjiang <geoffreychjiang@gmail.com>
2022-09-14 23:16:31 -0700

    added userid to create album

--- backend/routes/api/album.js
+++ backend/routes/api/album.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { Album } = require("../../db/models");
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
 
-router.post("/", async (req, res) => {
-  const { title, description, imageUrl } = req.body;
+router.post("/", restoreUser, async (req, res) => {
+  const { title, description, imageUrl, userId } = req.body;
+  const { user } = req;
+  const id = user.id;
   const newAlbum = await Album.create({
     title,
     description,
     imageUrl,
+    userId: id,
   });
 
   res.json(newAlbum);
