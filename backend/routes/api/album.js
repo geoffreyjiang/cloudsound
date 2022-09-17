c625734da5752746c049c7857a2eeb54e19ce910
geoffreyjiang <geoffreychjiang@gmail.com>
2022-09-17 11:35:38 -0700

    able to get albums and check if album exists

--- backend/routes/api/album.js
+++ backend/routes/api/album.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Album } = require("../../db/models");
+const { Album, User, Song } = require("../../db/models");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 
 router.post("/", restoreUser, async (req, res) => {
@@ -25,6 +25,42 @@ router.get("/current", restoreUser, async (req, res) => {
   res.json(albums);
 });
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const album = await Album.findOne({
+    where: { id },
+    include: [
+      {
+        model: User,
+        as: "Artist",
+        attributes: ["id", "username", "imageUrl"],
+      },
+      {
+        model: Song,
+        attributes: [
+          "id",
+          "userId",
+          "title",
+          "description",
+          "url",
+          "createdAt",
+          "updatedAt",
+          "imageUrl",
+        ],
+      },
+    ],
+  });
+
+  if (!album) {
+    res.status = 404;
+    res.json({
+      message: "Album does not exist",
+      statusCode: "404",
+    });
+  }
+
+  res.json(album);
+});
 router.get("/", async (req, res) => {
   const album = await Album.findAll();
   res.json(album);
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,6 +8,7 @@ const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const songsRouter = require("./songs.js");
 const albumRouter = require("./album.js");
+const artistRouter = require("./artist.js");
 router.use(restoreUser);
 
 router.use("/session", sessionRouter);
@@ -15,6 +16,7 @@ router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
 router.use("/songs", songsRouter);
 router.use("/albums", albumRouter);
+router.use("/artists", artistRouter);
 
 // router.get("/restore-user", (req, res) => {
 //   return res.json(req.user);
