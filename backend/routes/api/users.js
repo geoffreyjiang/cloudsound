45b5c39b7525fb28cafa2d6f1aed1e3094ff7c51
geoffreyjiang <geoffreychjiang@gmail.com>
2022-09-17 14:28:26 -0700

    able to get artist details

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { User } = require("../../db/models");
+const { User, Song, Album } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
+const { Op } = require("sequelize");
 const validateSignup = [
   check("email")
     .exists({ checkFalsy: true })
@@ -55,4 +56,40 @@ router.post("/", validateSignup, async (req, res) => {
     token,
   });
 });
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findOne({
+    where: { id },
+    attributes: ["id", "username", "imageUrl"],
+    include: [
+      {
+        model: Song,
+        attributes: ["imageUrl"],
+      },
+    ],
+  });
+
+  const totalSongs = await Song.count({
+    where: {
+      userId: Number(id),
+    },
+  });
+
+  user.dataValues.totalSongs = totalSongs;
+  const totalAlbums = await Album.count({
+    where: {
+      userId: Number(id),
+    },
+  });
+  user.dataValues.totalAlbums = totalAlbums;
+
+  if (!user) {
+    res
+      .status(404)
+      .json({ statusCode: 404, message: "Artist does not exists" });
+  }
+  res.json(user);
+});
+
 module.exports = router;
