5975cb614303e18f2031ee50b89eb2d752602b19
geoffreyjiang <geoffreychjiang@gmail.com>
2022-09-17 13:41:45 -0700

    able to delete comments

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -30,4 +30,31 @@ router.put("/:id", restoreUser, async (req, res, next) => {
   res.json(comment);
 });
 
+router.delete("/:id", restoreUser, async (req, res, next) => {
+  const { id } = req.params;
+  const { body } = req.body;
+  const { user } = req;
+  const current = user.toSafeObject();
+  const comment = await Comment.findOne({
+    where: { id },
+  });
+
+  if (!comment) {
+    res.status(404);
+    res.json({
+      message: "Comment couldn't be found",
+      statusCode: 404,
+    });
+  }
+  if (current.id === comment.userId) {
+    await comment.destroy();
+    res.json({
+      message: "Deleted",
+    });
+  } else {
+    const error = new Error("Invalid credentials");
+    throw error;
+  }
+});
+
 module.exports = router;
