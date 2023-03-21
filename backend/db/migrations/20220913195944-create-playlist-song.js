618020b0498e5b130ff54eafc0e2590923ea24c4
geoffreyjiang <geoffreychjiang@gmail.com>
2023-03-21 13:21:44 -0700

    started playlist backend

--- backend/db/migrations/20220913195944-create-playlist-song.js
+++ backend/db/migrations/20220913195944-create-playlist-song.js
@@ -1,30 +1,39 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('PlaylistSongs', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      playlistId: {
-        type: Sequelize.INTEGER
-      },
-      songId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('PlaylistSongs');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("PlaylistSongs", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            playlistId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: "Playlist", key: "id" },
+                onDelete: "cascade",
+            },
+            songId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "Song",
+                    key: "id",
+                },
+                onDelete: "cascade",
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("PlaylistSongs");
+    },
 };
--- frontend/src/components/Songs/SongDetail.js
+++ frontend/src/components/Songs/SongDetail.js
@@ -6,194 +6,172 @@ import { getComments, deleteComment } from "../../store/comments";
 import CreateComment from "../Comments";
 import "./songDetail.css";
 const SongDetail = () => {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  // const songs = useSelector((state) => state);
-  // console.log(id);
-  const user = useSelector((state) => state.session.user);
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    // const songs = useSelector((state) => state);
+    // console.log(id);
+    const user = useSelector((state) => state.session.user);
 
-  const history = useHistory();
-  const song = useSelector((state) => Object.values(state.songs));
-  const comments = useSelector((state) => Object.values(state.comments));
-  // console.log(comments);
-  // comments.forEach((el) => console.log(el));
+    const history = useHistory();
+    const song = useSelector((state) => Object.values(state.songs));
+    const comments = useSelector((state) => Object.values(state.comments));
+    // console.log(comments);
+    // comments.forEach((el) => console.log(el));
 
-  let commentId;
-  let allComments;
-  let commentUserId;
-  let userId;
-  // console.log(commentUserId);
-  // console.log(user.id);
-  let delBtn;
-  // const deleteCommentBtn = () => {
-  //   dispatch(deleteComment(commentId));
-  //   document.querySelector(".my-comments-section").remove();
-  //   window.location.reload();
-  // };
+    let commentId;
+    let allComments;
+    let commentUserId;
+    let userId;
+    // console.log(commentUserId);
+    // console.log(user.id);
+    let delBtn;
+    // const deleteCommentBtn = () => {
+    //   dispatch(deleteComment(commentId));
+    //   document.querySelector(".my-comments-section").remove();
+    //   window.location.reload();
+    // };
 
-  if (comments) {
-    let username;
-    // comments.forEach((el) => {});
-    allComments = comments.map((el) => {
-      // commentUserId = el.User.id;
-      let y;
-      commentId = el.id;
-      // el.User ? (y = el.User.username) : (y = "anon");
-      console.log(el);
-      // return (
-      //   <div className="my-comments-section comments-section">
-      //     <div>{el.body}</div>
-      //     <div>
-      //       By: {el.username}
-      //       <button
-      //         className="comment-btn"
-      //         onClick={async () => {
-      //           if (el.username !== user.username) {
-      //             alert("this is not your comment");
-      //           } else {
-      //             await dispatch(deleteComment(el.id));
-      //             history.push(`/songs/${id}`);
-      //           }
-      //         }}
-      //       >
-      //         Delete
-      //       </button>
-      //     </div>
-      //   </div>
-      // );
-      if (!user) {
-        return (
-          <div className="comments-section">
-            <div>{el.body}</div>
-            <div>By: {el.username}</div>
-          </div>
-        );
-      } else if (el.username !== user.username) {
-        return (
-          <div className="comments-section">
-            <div>{el.body}</div>
-            <div>By: {el.username}</div>
-          </div>
-        );
-      } else {
-        return (
-          <div className="my-comments-section comments-section">
-            <div>{el.body}</div>
-            <div>
-              By: {el.username}
-              <button
-                className="comment-btn"
-                onClick={async () => {
-                  await dispatch(deleteComment(el.id));
-                  // <Redirect replace to={`/songs/${id}`} />;
-                }}
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        );
-      }
-    });
-  }
-  const editBtn = () => {
-    if (!user || user.id !== userId) {
-      alert("This is not your song!");
-    } else if (user.id === userId) {
-      history.push(`/songs/${id}/edit`);
+    if (comments) {
+        let username;
+        // comments.forEach((el) => {});
+        allComments = comments.map((el) => {
+            // commentUserId = el.User.id;
+            let y;
+            commentId = el.id;
+
+            if (!user) {
+                return (
+                    <div className="comments-section">
+                        <div>{el.body}</div>
+                        <div>By: {el.username}</div>
+                    </div>
+                );
+            } else if (el.username !== user.username) {
+                return (
+                    <div className="comments-section">
+                        <div>{el.body}</div>
+                        <div>By: {el.username}</div>
+                    </div>
+                );
+            } else {
+                return (
+                    <div className="my-comments-section comments-section">
+                        <div>{el.body}</div>
+                        <div>
+                            By: {el.username}
+                            <button
+                                className="comment-btn"
+                                onClick={async () => {
+                                    await dispatch(deleteComment(el.id));
+                                    // <Redirect replace to={`/songs/${id}`} />;
+                                }}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                );
+            }
+        });
     }
-    // history.push(`/songs/${id}/edit`);
-  };
-  const deleteBtn = () => {
-    if (!user || user.id !== userId) {
-      alert("This is not your song!");
-    } else if (user.id === userId) {
-      dispatch(removeSong(id));
-      history.push(`/songs/`);
-    }
-  };
-  // const loadComments = () => {
-  //   dispatch(songById(id));
-  //   dispatch(getComments());
-  // };
-  // loadComments();
-  useEffect(() => {
-    const load = async () => {
-      // await dispatch(getAllSongs());
-      // loadComments();
-      dispatch(songById(id));
-
-      dispatch(getComments(id));
+    const editBtn = () => {
+        if (!user || user.id !== userId) {
+            alert("This is not your song!");
+        } else if (user.id === userId) {
+            history.push(`/songs/${id}/edit`);
+        }
+        // history.push(`/songs/${id}/edit`);
     };
-    load();
-  }, [dispatch, id]);
+    const deleteBtn = () => {
+        if (!user || user.id !== userId) {
+            alert("This is not your song!");
+        } else if (user.id === userId) {
+            dispatch(removeSong(id));
+            history.push(`/songs/`);
+        }
+    };
+    // const loadComments = () => {
+    //   dispatch(songById(id));
+    //   dispatch(getComments());
+    // };
+    // loadComments();
+    useEffect(() => {
+        const load = async () => {
+            // await dispatch(getAllSongs());
+            // loadComments();
+            dispatch(songById(id));
 
-  // console.log(userId);
-  const details = song.map((el, i) => {
-    // console.log(el);
-    userId = el.userId;
-    let uploadedBy;
-    if (!el.Artist) {
-      uploadedBy = "test";
-    } else {
-      uploadedBy = el.Artist.username;
-    }
+            dispatch(getComments(id));
+        };
+        load();
+    }, [dispatch, id]);
+
+    // console.log(userId);
+    const details = song.map((el, i) => {
+        // console.log(el);
+        userId = el.userId;
+        let uploadedBy;
+        if (!el.Artist) {
+            uploadedBy = "test";
+        } else {
+            uploadedBy = el.Artist.username;
+        }
+        return (
+            <>
+                <div className="song-card">
+                    <div className="song-img">
+                        <img
+                            className="detail-img"
+                            src={el.imageUrl}
+                            alt=""
+                            height="100x"
+                            width="80x"
+                        ></img>
+                    </div>
+                    <div className="song-info">
+                        <label className="detail-label">
+                            Title <h2>{el.title}</h2>
+                        </label>
+                        <label className="detail-label">
+                            Uploaded By <h2>{uploadedBy}</h2>
+                        </label>
+                        <label className="detail-label">
+                            Description<h2>{el.description}</h2>
+                        </label>
+
+                        {/* <Link to={`/songs/${el.id}/edit`}>Edit</Link> */}
+                        <button onClick={editBtn}>Edit</button>
+                        <button onClick={deleteBtn}>Delete</button>
+                        <audio controls src={el.url}>
+                            <a href={el.url}>Link</a>
+                        </audio>
+                    </div>
+                    {/* <div>Url:{el.url}</div> */}
+                </div>
+            </>
+        );
+    });
+
+    // loadComments();
     return (
-      <>
-        <div className="song-card">
-          <div className="song-img">
-            <img
-              className="detail-img"
-              src={el.imageUrl}
-              alt=""
-              height="100x"
-              width="80x"
-            ></img>
-          </div>
-          <div className="song-info">
-            <label className="detail-label">
-              Title <h2>{el.title}</h2>
-            </label>
-            <label className="detail-label">
-              Uploaded By <h2>{uploadedBy}</h2>
-            </label>
-            <label className="detail-label">
-              Description<h2>{el.description}</h2>
-            </label>
+        <>
+            <div key={id} className="song-containerer">
+                {details}
+            </div>
+            <section className="comment-section">
+                <h1>Comments</h1>
+                {allComments}
 
-            {/* <Link to={`/songs/${el.id}/edit`}>Edit</Link> */}
-            <button onClick={editBtn}>Edit</button>
-            <button onClick={deleteBtn}>Delete</button>
-            <audio controls src={el.url}>
-              <a href={el.url}>Link</a>
-            </audio>
-          </div>
-          {/* <div>Url:{el.url}</div> */}
-        </div>
-      </>
+                <br></br>
+                <br></br>
+                <h2>Leave a comment</h2>
+                <CreateComment />
+            </section>
+            <br></br>
+            <br></br>
+            <br></br>
+        </>
     );
-  });
-
-  // loadComments();
-  return (
-    <>
-      <div key={id} className="song-containerer">
-        {details}
-      </div>
-      <section className="comment-section">
-        <h1>Comments</h1>
-        {allComments}
-
-        <br></br>
-        <br></br>
-        <h2>Leave a comment</h2>
-        <CreateComment />
-      </section>
-      <br></br>
-      <br></br>
-      <br></br>
-    </>
-  );
 };
 
 export default SongDetail;
