01ec3506a404a56419c7a9bdcbea9083cf44bcf2
geoffreyjiang <geoffreychjiang@gmail.com>
2023-03-30 16:53:34 -0700

    able to get playlist id and songs on playlist page

--- backend/db/seeders/20220920025024-playlist.js
+++ backend/db/seeders/20220920025024-playlist.js
@@ -1,38 +1,41 @@
 "use strict";
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert("Playlists", [
-      {
-        userId: 1,
-        name: "The Playlist",
-        imageUrl: "image.url",
-        createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
-        updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      {
-        userId: 2,
-        name: "The other Playlist",
-        imageUrl: "image.url",
-        createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
-        updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      {
-        userId: 4,
-        name: "The best Playlist",
-        imageUrl: "image.url",
-        createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
-        updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-    ]);
-  },
+    async up(queryInterface, Sequelize) {
+        return queryInterface.bulkInsert("Playlists", [
+            {
+                userId: 1,
+                name: "The Playlist",
+                imageUrl:
+                    "https://cdn.pixabay.com/photo/2020/08/03/09/51/iphone-5459688__340.png",
+                createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+                updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+            },
+            {
+                userId: 2,
+                name: "The other Playlist",
+                imageUrl:
+                    "https://cdn.pixabay.com/photo/2017/08/06/12/54/headphones-2592263__340.jpg",
+                createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+                updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+            },
+            {
+                userId: 4,
+                name: "The best Playlist",
+                imageUrl:
+                    "https://cdn.pixabay.com/photo/2018/04/11/19/48/music-3311599__340.png",
+                createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+                updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+            },
+        ]);
+    },
 
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  },
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    },
 };
